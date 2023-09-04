caded4f8cd9d993b45c1fdd9cba9d65776cd9b63
Thu Hoai <hoaiphuongle194@gmail.com>
2023-09-04 11:21:42 +0700

    push code reactjs

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss';
+// để xóa ctrl+alt+ o xóa đi những cái ko import 
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to React Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
